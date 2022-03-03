bc2eb993d09f788285c3f4c3b420f6dd0102e760
fresh-ops <83802608+fresh-ops@users.noreply.github.com>
2022-03-03 16:13:41 +0700

    Added: command palette and show_msg method to ConsoleInterface class

--- shedlib/UI/ConsoleInterface.py
+++ shedlib/UI/ConsoleInterface.py
@@ -1,4 +1,5 @@
-from UserInterface import UserInterface
+from shedlib.UI.UserInterface import UserInterface
+import os
 
 
 class ConsoleInterface(UserInterface):
@@ -6,3 +7,19 @@ class ConsoleInterface(UserInterface):
    def __init__(self) -> None:
       """Initialization method"""
       super().__init__()
+      self.__com_palette = {
+         "show_msg": lambda x="Hello": self.show_msg(msg= x)
+      }
+
+   def update(self, command: str):
+      com = command.split(" ")[0]
+      args = command.replace(com+" ", "")
+      self.__com_palette[com](args)
+
+      return super().update()
+
+   def show_msg(self, msg):
+      """Write message in console"""
+      if len(msg) == 0:
+         msg = "Hello"
+      print(msg)
