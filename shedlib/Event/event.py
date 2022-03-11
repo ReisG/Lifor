be5366cf214bff0f6a7bbeaf62c1447b2827a63f
Куриленко Михаил Юрьевич <n-KurilenkoMI@ad.astu>
2022-03-11 18:49:02 +0700

    fixed class initializing and added desc for class

--- shedlib/Event/event.py
+++ shedlib/Event/event.py
@@ -1,10 +1,10 @@
-from Time.Time import Time
+from shedlib.Time.Time import Time
 
 
 class Event:
-    def __init__(self):
-        self.data = []
-
-    def initialize(self, identificator: int, name: str, start: Time, end: Time):
+    def __init__(self, identificator: int, name: str, start: Time, end: Time):
+        """Class for event"""
         self.data = [identificator, name, start, end]
 
+    def get_data(self):
+        return self.data
