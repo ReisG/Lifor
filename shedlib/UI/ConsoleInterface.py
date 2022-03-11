d8c39f1408c2f256911eadf5aa417cb82220c83a
fresh-ops <solovev-matveyka@inbox.ru>
2022-03-11 18:19:24 +0700

    Added: ConsoleInterface's method to write point in console

--- shedlib/UI/ConsoleInterface.py
+++ shedlib/UI/ConsoleInterface.py
@@ -2,19 +2,24 @@ from shedlib.UI.UserInterface import UserInterface
 
 
 class ConsoleInterface(UserInterface):
-   """Console user interface class"""
-   def __init__(self) -> None:
-      """Initialization method"""
-      super().__init__()
+    """Console user interface class"""
 
-   def update(self) -> None:
+    def __init__(self) -> None:
+        """Initialization method"""
+        super().__init__()
 
-      super().update()
+    def update(self) -> None:
+        super().update()
 
-   def show_msg(self, msg: str = "Hello, World!") -> None:
-      """Write message in console"""
-      print(msg)
+    def show_msg(self, msg: str = "Hello, World!") -> None:
+        """Write message in console"""
+        print(msg)
 
-   def get_log(self) -> str:
-      """Get users input from console"""
-      return input(">>>")
+    def show_point(self, name: str = "Nothin\'", last: int = 0) -> None:
+        """Write info about point in console"""
+        log = f"{name} will last next {last} sec."
+        self.show_msg(msg=log)
+
+    def get_log(self) -> str:
+        """Get users input from console"""
+        return input(">>>")
