158e991fb780524dc746a5e8ded67563606f1caf
ReisG <romanryabcev2006@gmail.com>
2022-03-28 14:41:35 +0700

    adding current week day and current time understanding

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+from shedlib.Parsing.parsing import Parser as Parser
+from shedlib.Time.Time import Time as Time
+import time
 
 
 def main():
-   """Main function"""
-   pass
+    """Main function"""
+
+    # Initializing progsses
+    WEEK_DAY = time.ctime().split()[0]
+    schedule = Parser.get(WEEK_DAY)
+
+    programm_end = False
+    cur_time = Time(0)
+    while not programm_end:
+        time_durty = time.strptime(time.ctime())
+        cur_time.set_in_hms_format(time_durty.tm_hour, time_durty.tm_min, time_durty.tm_sec)
+
+        print(cur_time.splited)
+        time.sleep(1)
+
 
 
 if __name__ == "__main__":
-   # Program entry
-   main()
+    main()
+    quit()
