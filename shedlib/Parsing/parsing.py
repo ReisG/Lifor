08e099bbef5ff91e0ee9829cf245e0ab86be5c75
ReisG <romanryabcev2006@gmail.com>
2022-03-29 16:07:39 +0700

    completing making Parser class more private

--- shedlib/Parsing/parsing.py
+++ shedlib/Parsing/parsing.py
@@ -39,6 +39,7 @@ class Parser:
             event = file.readline()
             while (len(event) > 1 or id == 0):
                 event = event.split(Parser.sep_symb)
+                event = event.split(Parser.__sep_symb)
                 result.append(Event( id, event[0], Time(int(event[1])), Time(int(event[2])) ))
                 id += 1
                 event = file.readline()
