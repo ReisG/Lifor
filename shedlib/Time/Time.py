8f15e740a1db1f9b54e8547b154c8f3af80d669a
ReisG <romanryabcev2006@gmail.com>
2022-03-29 15:47:34 +0700

    added Time class documentation

--- shedlib/Time/Time.py
+++ shedlib/Time/Time.py
@@ -1,19 +1,25 @@
 class Time:
+    ''' Object for storing time '''
+
     __slots__ = ('__seconds',)
 
-    def __init__(self, seconds=0):
+    def __init__(self, seconds:int=0):
         self.__seconds = seconds
 
     @property
     def splited(self):
-        """ Returns a list in format [hours, minutes, seconds] """
+        ''' Returns a list in format [hours, minutes, seconds] '''
         return [self.__seconds // 3600, self.__seconds // 60 % 60, self.__seconds % 60]
 
     def get_seconds(self):
         return self.__seconds
 
-    def set_in_hms_format(self, hours, minutes, seconds):
-        ''' Method for setting time in format hours:minutes:seconds '''
+    def set_in_hms_format(self, hours:int, minutes:int, seconds:int):
+        ''' This method will help you to write data in object in hours:minutes:seconds format
+
+        Parameters:
+            int hours, minutes, seconds: parts
+        '''
         self.__seconds = hours*3600 + minutes*60 + seconds
 
     def __sub__(self, other):
@@ -31,12 +37,12 @@ class Time:
     def __lt__(self, other):
         return self.__seconds < other.__seconds
 
-    def __le__(self, other):
+    def __le__(self, other:Time):
         return self < other or self == other
 
-    def __ne__(self, other):
+    def __ne__(self, other:Time):
         return not self == other
-    
+
     def __str__(self) -> str:
         t = self.splited
         res = ""
