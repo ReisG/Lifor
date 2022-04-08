8370b212c907cd569c736c7578e368f66f80d7bb
fresh-ops <solovev-matveyka@inbox.ru>
2022-04-08 18:18:40 +0700

    Added: program file Now main file only starts program. All logic of cycle is in program.py

---
+++ shedlib/program.py
@@ -0,0 +1,47 @@
+from shedlib.ui.ConsoleInterface import ConsoleInterface as ConsoleInterface
+from shedlib.parsing.parsing import Parser as Parser
+from shedlib.time.Time import Time as Time
+import time
+
+
+class LiforApp:
+    """Main application class"""
+
+    __slots__ = ('__schedule', '__cli', '__cur_time')
+
+    def __init__(self):
+        """Initialize object"""
+        __WEEK_DAY = time.ctime().split()[0]  # must be private
+
+        self.__schedule = Parser.get(__WEEK_DAY) # private
+        self.__cli = ConsoleInterface() # private
+        self.__cur_time = Time(0) # private
+
+    def run(self) -> None:
+        """Start a main program cycle"""
+        program_end = False
+
+        while not program_end:
+            self.update_time()
+
+            self.make_scene()
+
+            self.__cli.update()
+            time.sleep(1)
+
+    def __update_time(self) -> None:
+        """Update current time"""
+        time_durty = time.strptime(time.ctime())
+        self.__cur_time.set_in_hms_format(time_durty.tm_hour, time_durty.tm_min, time_durty.tm_sec)
+
+    def __make_scene(self) -> None:
+        """Parse activities and send them to interface"""
+        for activity in self.__schedule:
+            if (self.__cur_time <= activity.start):
+                # activity hasn't started yet
+                self.__cli.append_points(activity.name, False, (activity.start - self.__cur_time))
+            elif (activity.start <= self.__cur_time <= activity.end):
+                # activity is already running
+                self.__cli.append_points(activity.name, True, (activity.end - self.__cur_time))
+            # otherwise we don't send it to Interface
+
